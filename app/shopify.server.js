b814ff08472d08db596250d39ae21c4bee57e590
ananya-cedcoss <ananyashukla@makewebbetter.com>
2025-10-08 13:30:39 +0530

    commit

--- app/shopify.server.js
+++ app/shopify.server.js
@@ -1,57 +1,28 @@
-import "@shopify/shopify-app-remix/adapters/node";
+import "@shopify/shopify-app-react-router/adapters/node";
 import {
   ApiVersion,
   AppDistribution,
   shopifyApp,
-  BillingInterval
-} from "@shopify/shopify-app-remix/server";
+} from "@shopify/shopify-app-react-router/server";
 import { PrismaSessionStorage } from "@shopify/shopify-app-session-storage-prisma";
 import prisma from "./db.server";
-
-export const MONTHLY_PLAN = 'Monthly subscription';
-export const ANNUAL_PLAN = 'Annual subscription';
-
 
 const shopify = shopifyApp({
   apiKey: process.env.SHOPIFY_API_KEY,
   apiSecretKey: process.env.SHOPIFY_API_SECRET || "",
-  apiVersion: ApiVersion.January25,
+  apiVersion: ApiVersion.October25,
   scopes: process.env.SCOPES?.split(","),
   appUrl: process.env.SHOPIFY_APP_URL || "",
   authPathPrefix: "/auth",
   sessionStorage: new PrismaSessionStorage(prisma),
   distribution: AppDistribution.AppStore,
-    billing: {
-    [MONTHLY_PLAN]: {
-      lineItems: [
-        {
-          amount: 10,
-          currencyCode: 'INR',
-          interval: BillingInterval.Every30Days,
-        }
-      ],
-    },
-    [ANNUAL_PLAN]: {
-      lineItems: [
-        {
-          amount: 100,
-          currencyCode: 'INR',
-          interval: BillingInterval.Annual,
-        }
-      ],
-    },
-  },
-  future: {
-    unstable_newEmbeddedAuthStrategy: true,
-    removeRest: true,
-  },
   ...(process.env.SHOP_CUSTOM_DOMAIN
     ? { customShopDomains: [process.env.SHOP_CUSTOM_DOMAIN] }
     : {}),
 });
 
 export default shopify;
-export const apiVersion = ApiVersion.January25;
+export const apiVersion = ApiVersion.October25;
 export const addDocumentResponseHeaders = shopify.addDocumentResponseHeaders;
 export const authenticate = shopify.authenticate;
 export const unauthenticated = shopify.unauthenticated;
